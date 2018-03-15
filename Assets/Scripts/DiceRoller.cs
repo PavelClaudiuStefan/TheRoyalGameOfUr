97bf84f5c834016cd2dd0d2ce1b22ba7fa21ee18
Pavel Claudiu Stefan <pavelclaudiu96@gmail.com>
2018-03-15 02:04:37 +0200

    Added game pieces, dice roller and basic advancing of pieces

---
+++ Assets/Scripts/DiceRoller.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceRoller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        DiceValues = new int[4];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public int[] DiceValues;
+    public int DiceSum;
+
+    public Text DiceDisplay;
+
+    public Sprite DiceImageOne;
+    public Sprite DiceImageZero;
+
+    public void NewTurn()
+    {
+
+    }
+
+    public void RollDice()
+    {
+        // In The Royal Game of Ur, there are 4 dices (tetrahedons), which have half the faces with a value of 1 and the rest of 0
+
+        DiceSum = 0;
+        for (int i = 0; i < DiceValues.Length; i++)
+        {
+            DiceValues[i] = Random.Range(0, 2);
+            DiceSum += DiceValues[i];
+
+            if (DiceValues[i] == 0)
+            {
+                this.transform.GetChild(i).GetComponent<Image>().sprite = DiceImageZero;
+            } else
+            {
+                this.transform.GetChild(i).GetComponent<Image>().sprite = DiceImageOne;
+            }
+        }
+
+        DiceDisplay.text = DiceSum.ToString();
+    }
+
+}
---
+++ Assets/Scripts/PlayerPiece.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPiece : MonoBehaviour {
+
+    DiceRoller diceRoller;
+	GameTile currentTile;
+
+	public GameTile StartingTile;
+
+	// Use this for initialization
+	void Start () {
+        diceRoller = GameObject.FindObjectOfType<DiceRoller>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseUp()
+    {
+        //TODO - Get out if click is on UI
+
+        int spacesToMove = diceRoller.DiceSum;
+
+		GameTile finalTile = currentTile;
+
+        for (int i = 0; i < spacesToMove; i++)
+        {
+            if (finalTile == null)
+            {
+                finalTile = StartingTile;
+            } else
+            {
+                if (finalTile.NextTiles == null || finalTile.NextTiles.Length == 0)
+                {
+                    // TODO - Implement scoring
+                    Debug.Log("Point!");
+                    Destroy(gameObject);
+                    return;
+                }
+                else if (finalTile.NextTiles.Length > 1)
+                {
+                    // TODO - Implement nextTile choosing based on player
+                    finalTile = finalTile.NextTiles[0];
+                }
+                else
+                {
+                    finalTile = finalTile.NextTiles[0];
+                }
+            }
+        }
+
+        if(finalTile == null)
+        {
+            return;
+        }
+
+        // Send piece to the final tile
+
+        this.transform.position = finalTile.transform.position;
+        currentTile = finalTile;
+    }
+}
